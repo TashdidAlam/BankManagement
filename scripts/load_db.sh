18520858d03f88f655fb2b0be11c1e30dca43740
Tashdid_Alam <130019268+TashdidAlam@users.noreply.github.com>
2023-06-12 19:40:01 +0600

    Update load_db.sh

--- scripts/load_db.sh
+++ scripts/load_db.sh
@@ -1,1 +1,1 @@
-mysql -h tashdid-db.czk1aergjpmh.eu-west-2.rds.amazonaws.com -u root --password="12345678" library < /tmp/blmsdb.sql 
+mysql -h tashdid-db.czk1aergjpmh.eu-west-2.rds.amazonaws.com -u root --password="12345678" blmsdb < /tmp/blmsdb.sql 
