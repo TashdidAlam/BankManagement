be541c453fc4bac4b66b6e2fd32dc7904599a057
Tashdid_Alam <130019268+TashdidAlam@users.noreply.github.com>
2023-06-12 11:13:19 +0600

    Update config.php

--- banker/includes/config.php
+++ banker/includes/config.php
@@ -2,7 +2,7 @@
 //time zone
 date_default_timezone_set('Asia/Kolkata');
 //database connection
-$con=mysqli_connect("localhost","root","","blmsdb");
+$con=mysqli_connect("localhost","root","12345678","blmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
